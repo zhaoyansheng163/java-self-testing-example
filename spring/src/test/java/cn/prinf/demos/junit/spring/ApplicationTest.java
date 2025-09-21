e88d82adee388665a071435625af82add86b05c7
jansenzhao <zhaoyansheng163@163.com>
2025-09-21 16:40:35 +0800

    添加说明

--- spring/src/test/java/cn/prinf/demos/junit/spring/ApplicationTest.java
+++ spring/src/test/java/cn/prinf/demos/junit/spring/ApplicationTest.java
@@ -6,6 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+/**
+ * 注意这里用的是内存数据库
+ * 跑这个类  会自动跑两个用例，先 should_add_user 再 should_list_users。 顺序是按照名称排序的。should_list_users这个会查到一条插入的
+ * 单独跑 should_list_users 查询到的是0条记录
+ */
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
         classes = {Application.class},
@@ -19,7 +26,8 @@ public class ApplicationTest {
 
     @Test
     public void should_list_users() {
-        userService.listAll();
+        List<User> users = userService.listAll();
+        System.out.println(users);
     }
 
     @Test
